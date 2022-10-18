a0efa899c25518525a07ba2ee6e0b6ec2df2e982
Mohammed Hossane <mohammedhossane@Mohammeds-MBP.lan>
2022-10-17 20:22:52 -0700

    replicated original challenge to reactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
 import './App.css';
 
-function App() {
+function Buttons() {
+  function playAnimation() {
+    //selects the "img" and changes the properties to a 3 second animation that goes on infinently. 
+    document.querySelector("img").style.setProperty("animation", ".2s ease 0s infinite normal none running img")
+  }
+
+  function stopAnimation() {
+    document.querySelector("img").style.setProperty("animation", ".2s ease 0s 0 normal none running img")
+  }
+  return(
+  <Fragment>
+    <button onClick={playAnimation} id="animationbutton" class="box" > Start Animation </button>
+
+    <button onClick={stopAnimation} id="animationbuttons" class="box" > End Animation </button>
+  </Fragment>
+  )
+}
+
+export {Buttons}
+function Creed() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="bigoutsidediv">
+
+        <header class="theposition">
+          <h1><b class="animate-charcter">SEALS</b></h1>
+        </header>
+
+        <div class="box">
+
+          <div class="texta">
+
+            <h3><b class="animate-charcters"> SEALS Challenge 1</b></h3>
+
+            <h5> What Are The Top 3 Leadership Creeds you Have Applied After SCLA? </h5>
+
+          </div>
+
+          <div class="texta">
+
+            <ul>
+              <li>
+                <b class="animate-charctersa">I will not lie, cheat, or steal.</b>
+              </li>
+
+              <p> In my APUSH class, many around me decide to cheat through exams and quizzes,
+                I have had many opportunities to do the same, although I choose not to.
+                If I cheat myself now, I will also cheat myself during the AP exam, where there is
+                no chance of cheating.</p>
+
+              <li>
+                <b class="animate-charctersa"> I will think outside the box.</b>
+              </li>
+
+              <p> After SCLA, I have always tried to think outside of the box. An example of me thinking
+                outside of the box
+                is in my physics class. We were assigned to create a popsicle stick bazooka and
+                consistently hit an object four times
+                in a row. Many other groups chose to create complicated and sophisticated designs and
+                failed to meet the goal. Instead
+                I made a straightforward design that was, in reality, a slingshot. I hit the object four
+                times and left with an A.
+              </p>
+
+              <li>
+                <b class="animate-charctersa">I believe nothing is hard just a lot of work.</b>
+              </li>
+
+              <p id="third-p-tag"> The idealogy of believing nothing is hard, just a lot of work, has stuck with me after
+                SCLA. I have applied this thinking into all most everything I do, whether it's school,
+                games, or general. For example, APUSH is not hard; it is just a lot of reading. Or a
+                game like VALORANT, VALORANT is not difficult; it just requires a lot of playing to
+                develop your skills.
+              </p>
+
+            </ul>
+          </div>
+        </div>
+        <img class="wickedinsane" src="https://cdn.betterttv.net/emote/6076964939b5010444cfee3c/3x" alt="bruh no moving from" />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Creed;
