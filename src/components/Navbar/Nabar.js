0462944096c416023a785d6a5f7e9e522ade66e6
rob <robsanchez1772@gmail.com>
2022-10-18 17:41:52 -0700

    added navbar

---
+++ src/components/Navbar/Nabar.js
@@ -0,0 +1,18 @@
+/**
+ * Roberto Sanchrz
+ * robsanchez17772@gmail.com
+ *
+ * Description:
+ * Renders a navigation bar with the links to other pages on my site
+ */
+export default function NavBar() {
+  return (
+    <div
+      style={{
+        // background-color
+        backgroundColor: "red",
+        height: 50,
+      }}
+    ></div>
+  );
+}
